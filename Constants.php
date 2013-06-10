736b76fbc614e239735717ee2d481b1593ec3290
Shanjeef/JBYung <shanjeef.satchi@xtremelabs.com>
2013-06-10 14:34:03 -0400

    EmirH: added ability to add items to databases

---
+++ Constants.php
@@ -0,0 +1,62 @@
+<?php 
+
+$URL = "localhost";
+$USER_NAME = "root";
+$PASSWORD = "root";
+$DATABASE = "DeviceInformation";
+
+$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY = "portraitScreenDetails";
+$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY = "landscapeScreenDetails";
+$REQUEST_VERSION_CODE_NAME_KEY = "versionCodeName";
+$REQUEST_VERSION_INCREMENTAL_KEY = "versionIncremental";
+$REQUEST_VERSION_RELEASE_KEY = "versionRelease";
+$REQUEST_VERSION_SDK_STRING_KEY = "versionSdkString";
+$REQUEST_VERSION_SDK_INTEGER_KEY = "versionSdkInteger";
+$REQUEST_BOARD_KEY = "board";
+$REQUEST_BOOT_LOADER_KEY = "bootLoader";
+$REQUEST_BRAND_KEY = "brand";
+$REQUEST_CPU_ABI_KEY = "cpuAbi";
+$REQUEST_CPU_ABI_2_KEY = "cpuAbi2";
+$REQUEST_DEVICE_KEY = "device";
+$REQUEST_DISPLAY_KEY = "display";
+$REQUEST_FINGER_PRINT_KEY = "fingerPrint";
+$REQUEST_HARDWARE_KEY = "hardwade";
+$REQUEST_HOST_KEY = "host";
+$REQUEST_DEVICE_ID_KEY = "deviceId";
+$REQUEST_MANUFACTURER_KEY = "manufacturer";
+$REQUEST_PRODUCT_KEY = "product";
+$REQUEST_RADIO_KEY = "radio";
+$REQUEST_TAGS_KEY = "tags";
+$REQUEST_TIME_KEY = "time";
+$REQUEST_TYPE_KEY = "type";
+$REQUEST_UNKNOWN_KEY = "unkown";
+$REQUEST_USER_KEY = "user";
+$REQUEST_DENSITY_NAME_KEY = "densityName";
+$REQUEST_SCREEN_SIZE_KEY = "screenSize";
+$REQUEST_DENSITY_KEY = "density";
+$REQUEST_XDPI_KEY = "xdpi";
+$REQUEST_YDPI_KEY = "ydpi";
+$REQUEST_COUNT_KEY = "ydpi";
+
+
+
+$REQUEST_DEVICE_PIXEL_HEIGHT_KEY = "devicePixelHeight";
+$REQUEST_DEVICE_PIXEL_WIDTH_KEY = "devicePixelWidth";
+$REQUEST_WINDOW_PIXEL_HEIGHT_KEY = "windowPixelHeight";
+$REQUEST_WINDOW_PIXEL_WIDTH_KEY = "windowPixelWidth";
+$REQUEST_CONTENT_VIEW_PIXEL_HEIGHT_KEY = "contentViewPixelHeight";
+$REQUEST_CONTENT_VIEW_PIXEL_WIDTH_KEY = "contentViewPixelWidth";
+$REQUEST_NAV_BAR_HEIGHT_KEY = "navBarHeight";
+$REQUEST_NAV_BAR_WIDTH_KEY = "navBarWidth";
+$REQUEST_STATUS_BAR_HEIGHT_KEY = "statusBarHeight";
+$REQUEST_TITLE_BAR_HEIGHT_KEY = "titleBarHeight";
+
+$TABLE_NAME_DEVICE_INFORMATION = "deviceInformation";
+$TABLE_NAME_SCREEN_DETAILS = "screenDetails";
+$TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID = "deviceInformationId";
+$TABLE_COLUMN_NAME_SCREEN_SIZE_ID = "screenSizeId";
+$TABLE_COLUMN_NAME_IS_PORTRAIT = "isPortrait";
+$TABLE_COLUMN_NAME_COUNT = "count";
+
+$RESPONSE_SUCCESS_KEY = "success";
+?>
--- UploadScreenSize.php
+++ UploadScreenSize.php
@@ -1,68 +1,18 @@
 <?php
 
+include 'Constants.php';
 
-$URL = "localhost";
-$USER_NAME = "root";
-$PASSWORD = "root";
-$DATABASE = "DeviceInformation";
+$data = file_get_contents('php://input');
+$json = json_decode($data, true);
 
-$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY = "portraitScreenDetails";
-$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY = "landscapeScreenDetails";
-$REQUEST_VERSION_CODE_NAME_KEY = "versionCodeName";
-$REQUEST_VERSION_INCREMENTAL_KEY = "versionIncremental";
-$REQUEST_VERSION_RELEASE_KEY = "versionRelease";
-$REQUEST_VERSION_SDK_STRING_KEY = "versionSdkString";
-$REQUEST_VERSION_SDK_INTEGER_KEY = "versionSdkInteger";
-$REQUEST_BOARD_KEY = "board";
-$REQUEST_BOOT_LOADER_KEY = "bootLoader";
-$REQUEST_BRAND_KEY = "brand";
-$REQUEST_CPU_ABI_KEY = "cpuAbi";
-$REQUEST_CPU_ABI_2_KEY = "cpuAbi2";
-$REQUEST_DEVICE_KEY = "device";
-$REQUEST_DISPLAY_KEY = "display";
-$REQUEST_FINGER_PRINT_KEY = "fingerPrint";
-$REQUEST_HARDWARE_KEY = "hardwade";
-$REQUEST_HOST_KEY = "host";
-$REQUEST_DEVICE_ID_KEY = "deviceId";
-$REQUEST_MANUFACTURER_KEY = "manufacturer";
-$REQUEST_PRODUCT_KEY = "product";
-$REQUEST_RADIO_KEY = "radio";
-$REQUEST_TAGS_KEY = "tags";
-$REQUEST_TIME_KEY = "time";
-$REQUEST_TYPE_KEY = "type";
-$REQUEST_UNKNOWN_KEY = "unkown";
-$REQUEST_USER_KEY = "user";
-$REQUEST_DENSITY_NAME_KEY = "densityName";
-$REQUEST_SCREEN_SIZE_KEY = "screenSize";
-$REQUEST_DENSITY_KEY = "density";
-$REQUEST_XDPI_KEY = "xdpi";
-$REQUEST_YDPI_KEY = "ydpi";
-
-
-
-$REQUEST_DEVICE_PIXEL_HEIGHT_KEY = "devicePixelHeight";
-$REQUEST_DEVICE_PIXEL_WIDTH_KEY = "devicePixelWidth";
-$REQUEST_WINDOW_PIXEL_HEIGHT_KEY = "windowPixelHeight";
-$REQUEST_WINDOW_PIXEL_WIDTH_KEY = "windowPixelWidth";
-$REQUEST_CONTENT_VIEW_PIXEL_HEIGHT_KEY = "contentViewPixelHeight";
-$REQUEST_CONTENT_VIEW_PIXEL_WIDTH_KEY = "contentViewPixelWidth";
-$REQUEST_NAV_BAR_HEIGHT_KEY = "navBarHeight";
-$REQUEST_NAV_BAR_WIDTH_KEY = "navBarWidth";
-$REQUEST_STATUS_BAR_HEIGHT_KEY = "statusBarHeight";
-$REQUEST_TITLE_BAR_HEIGHT_KEY = "titleBarHeight";
-
-$TABLE_NAME_DEVICE_INFORMATION = "deviceInformation";
-$TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID = "deviceInformationId";
-$TABLE_COLUMN_NAME_SCREEN_SIZE_ID = "screenSizeId";
-$TABLE_COLUMN_NAME_SCREEN_SIZE_ID = "deviceInformationId";
-$TABLE_COLUMN_NAME_IS_PORTRAIT = "isPortrait";
-
-$RESPONSE_SUCCESS_KEY = "success";
 
 header('Content-type: application/json');
 
-function checkScreenDetails($screenDetails)
+function isScreenDetailsSet($screenDetails)
 {
+
+	include 'Constants.php';
+
 	return isset($screenDetails[$REQUEST_DEVICE_PIXEL_HEIGHT_KEY]) &&
 	isset($screenDetails[$REQUEST_DEVICE_PIXEL_WIDTH_KEY]) &&
 	isset($screenDetails[$REQUEST_WINDOW_PIXEL_HEIGHT_KEY]) &&
@@ -77,6 +27,9 @@ function checkScreenDetails($screenDetails)
 
 function getScreenDetailsInsertQuery($database, $deviceInformationId, $isPortrait, $screenDetails)
 {
+
+	include 'Constants.php';
+
 	$devicePixelHeight = $database->escape_string($screenDetails[$REQUEST_DEVICE_PIXEL_HEIGHT_KEY]);
 	$devicePixelWidth = $database->escape_string($screenDetails[$REQUEST_DEVICE_PIXEL_WIDTH_KEY]);
 	$windowPixelHeight = $database->escape_string($screenDetails[$REQUEST_WINDOW_PIXEL_HEIGHT_KEY]);
@@ -89,8 +42,8 @@ function getScreenDetailsInsertQuery($database, $deviceInformationId, $isPortrai
 	$titleBarHeight = $database->escape_string($screenDetails[$REQUEST_TITLE_BAR_HEIGHT_KEY]);
 
 
-	$insertDeviceInformation =  "INSERT INTO $TABLE_NAME_DEVICE_INFORMATION (" .
-	"$TABLE_COLUMN_NAME_SCREEN_SIZE_ID, " .
+	$insertDeviceInformation =  "INSERT INTO $TABLE_NAME_SCREEN_DETAILS (" .
+	"$TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID, " .
 	"$TABLE_COLUMN_NAME_IS_PORTRAIT, " . 
 	"$REQUEST_DEVICE_PIXEL_HEIGHT_KEY, " .
 	"$REQUEST_DEVICE_PIXEL_WIDTH_KEY, " .
@@ -117,79 +70,81 @@ function getScreenDetailsInsertQuery($database, $deviceInformationId, $isPortrai
 	"$titleBarHeight".
 	");";
 
+	return $insertDeviceInformation;
+
 
 }
 
 if (
-isset($_POST[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
-isset($_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY]) &&
-isset($_POST[$REQUEST_VERSION_CODE_NAME_KEY]) &&
-isset($_POST[$REQUEST_VERSION_INCREMENTAL_KEY]) &&
-isset($_POST[$REQUEST_VERSION_RELEASE_KEY]) &&
-isset($_POST[$REQUEST_VERSION_SDK_STRING_KEY]) &&
-isset($_POST[$REQUEST_VERSION_SDK_INTEGER_KEY]) &&
-isset($_POST[$REQUEST_BOARD_KEY]) &&
-isset($_POST[$REQUEST_BOOT_LOADER_KEY]) &&
-isset($_POST[$REQUEST_BRAND_KEY]) &&
-isset($_POST[$REQUEST_CPU_ABI_KEY]) &&
-isset($_POST[$REQUEST_CPU_ABI_2_KEY]) &&
-isset($_POST[$REQUEST_DEVICE_KEY]) &&
-isset($_POST[$REQUEST_DISPLAY_KEY]) &&
-isset($_POST[$REQUEST_FINGER_PRINT_KEY]) &&
-isset($_POST[$REQUEST_HARDWARE_KEY]) &&
-isset($_POST[$REQUEST_HOST_KEY]) &&
-isset($_POST[$REQUEST_DEVICE_ID_KEY]) &&
-isset($_POST[$REQUEST_MANUFACTURER_KEY]) &&
-isset($_POST[$REQUEST_PRODUCT_KEY]) &&
-isset($_POST[$REQUEST_RADIO_KEY]) &&
-isset($_POST[$REQUEST_TAGS_KEY]) &&
-isset($_POST[$REQUEST_TIME_KEY]) &&
-isset($_POST[$REQUEST_TYPE_KEY]) &&
-isset($_POST[$REQUEST_UNKNOWN_KEY]) &&
-isset($_POST[$REQUEST_USER_KEY]) &&
-isset($_POST[$REQUEST_DENSITY_NAME_KEY]) &&
-isset($_POST[$REQUEST_SCREEN_SIZE_KEY]) &&
-isset($_POST[$REQUEST_DENSITY_KEY]) &&
-isset($_POST[$REQUEST_XDPI_KEY]) &&
-isset($_POST[$REQUEST_YDPI_KEY]) &&
-is_array($_POST[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
-is_array($_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY]) &&
-isScreenDetailsSet($_POST[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
-isScreenDetailsSet($_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY])
+isset($json[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
+isset($json[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY]) &&
+isset($json[$REQUEST_VERSION_CODE_NAME_KEY]) &&
+isset($json[$REQUEST_VERSION_INCREMENTAL_KEY]) &&
+isset($json[$REQUEST_VERSION_RELEASE_KEY]) &&
+isset($json[$REQUEST_VERSION_SDK_STRING_KEY]) &&
+isset($json[$REQUEST_VERSION_SDK_INTEGER_KEY]) &&
+isset($json[$REQUEST_BOARD_KEY]) &&
+isset($json[$REQUEST_BOOT_LOADER_KEY]) &&
+isset($json[$REQUEST_BRAND_KEY]) &&
+isset($json[$REQUEST_CPU_ABI_KEY]) &&
+isset($json[$REQUEST_CPU_ABI_2_KEY]) &&
+isset($json[$REQUEST_DEVICE_KEY]) &&
+isset($json[$REQUEST_DISPLAY_KEY]) &&
+isset($json[$REQUEST_FINGER_PRINT_KEY]) &&
+isset($json[$REQUEST_HARDWARE_KEY]) &&
+isset($json[$REQUEST_HOST_KEY]) &&
+isset($json[$REQUEST_DEVICE_ID_KEY]) &&
+isset($json[$REQUEST_MANUFACTURER_KEY]) &&
+isset($json[$REQUEST_PRODUCT_KEY]) &&
+isset($json[$REQUEST_RADIO_KEY]) &&
+isset($json[$REQUEST_TAGS_KEY]) &&
+isset($json[$REQUEST_TIME_KEY]) &&
+isset($json[$REQUEST_TYPE_KEY]) &&
+isset($json[$REQUEST_UNKNOWN_KEY]) &&
+isset($json[$REQUEST_USER_KEY]) &&
+isset($json[$REQUEST_DENSITY_NAME_KEY]) &&
+isset($json[$REQUEST_SCREEN_SIZE_KEY]) &&
+isset($json[$REQUEST_DENSITY_KEY]) &&
+isset($json[$REQUEST_XDPI_KEY]) &&
+isset($json[$REQUEST_YDPI_KEY]) &&
+is_array($json[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
+is_array($json[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY]) &&
+isScreenDetailsSet($json[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY]) &&
+isScreenDetailsSet($json[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY])
 ) {
 	$database = new mysqli($URL, $USER_NAME, $PASSWORD, $DATABASE);
 
-	$portraitScreenDetails = $_POST[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY];
-	$landscapeScreenDetails = $_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY];
-	$versionCodeName = $_POST[$REQUEST_VERSION_CODE_NAME_KEY];
-	$versionIncremental = $_POST[$REQUEST_VERSION_INCREMENTAL_KEY];
-	$versionRelease = $database->escape_string($_POST[$REQUEST_VERSION_RELEASE_KEY]);
-	$versionSdkString = $database->escape_string($_POST[$REQUEST_VERSION_SDK_STRING_KEY]);
-	$versionSdkInteger = $database->escape_string($_POST[$REQUEST_VERSION_SDK_INTEGER_KEY]);
-	$board =$database->escape_string( $_POST[$REQUEST_BOARD_KEY]);
-	$bootLoader = $database->escape_string($_POST[$REQUEST_BOOT_LOADER_KEY]);
-	$brand = $database->escape_string($_POST[$REQUEST_BRAND_KEY]);
-	$cpuAbi = $database->escape_string($_POST[$REQUEST_CPU_ABI_KEY]);
-	$cpuAbi2 = $database->escape_string($_POST[$REQUEST_CPU_ABI_2_KEY]);
-	$device = $database->escape_string($_POST[$REQUEST_DEVICE_KEY]);
-	$display = $database->escape_string($_POST[$REQUEST_DISPLAY_KEY]);
-	$fingerPrint = $database->escape_string($_POST[$REQUEST_FINGER_PRINT_KEY]);
-	$hardwade = $database->escape_string($_POST[$REQUEST_HARDWARE_KEY]);
-	$host = $database->escape_string($_POST[$REQUEST_HOST_KEY]);
-	$deviceId = $database->escape_string($_POST[$REQUEST_DEVICE_ID_KEY]);
-	$manufacturer = $database->escape_string($_POST[$REQUEST_MANUFACTURER_KEY]);
-	$product = $database->escape_string($_POST[$REQUEST_PRODUCT_KEY]);
-	$radio = $database->escape_string($_POST[$REQUEST_RADIO_KEY]);
-	$tags = $database->escape_string($_POST[$REQUEST_TAGS_KEY]);
-	$time = $database->escape_string($_POST[$REQUEST_TIME_KEY]);
-	$type = $database->escape_string($_POST[$REQUEST_TYPE_KEY]);
-	$unkown = $database->escape_string($_POST[$REQUEST_UNKNOWN_KEY]);
-	$user = $database->escape_string($_POST[$REQUEST_USER_KEY]);
-	$densityName = $database->escape_string($_POST[$REQUEST_DENSITY_NAME_KEY]);
-	$screenSize = $database->escape_string($_POST[$REQUEST_SCREEN_SIZE_KEY]);
-	$density = $database->escape_string($_POST[$REQUEST_DENSITY_KEY]);
-	$xdpi = $database->escape_string($_POST[$REQUEST_XDPI_KEY]);
-	$ydpi = $database->escape_string($_POST[$REQUEST_YDPI_KEY]);
+	$portraitScreenDetails = $json[$REQUEST_PORTRAIT_SCREEN_DETAILS_KEY];
+	$landscapeScreenDetails = $json[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY];
+	$versionCodeName = $json[$REQUEST_VERSION_CODE_NAME_KEY];
+	$versionIncremental = $json[$REQUEST_VERSION_INCREMENTAL_KEY];
+	$versionRelease = $database->escape_string($json[$REQUEST_VERSION_RELEASE_KEY]);
+	$versionSdkString = $database->escape_string($json[$REQUEST_VERSION_SDK_STRING_KEY]);
+	$versionSdkInteger = $database->escape_string($json[$REQUEST_VERSION_SDK_INTEGER_KEY]);
+	$board =$database->escape_string( $json[$REQUEST_BOARD_KEY]);
+	$bootLoader = $database->escape_string($json[$REQUEST_BOOT_LOADER_KEY]);
+	$brand = $database->escape_string($json[$REQUEST_BRAND_KEY]);
+	$cpuAbi = $database->escape_string($json[$REQUEST_CPU_ABI_KEY]);
+	$cpuAbi2 = $database->escape_string($json[$REQUEST_CPU_ABI_2_KEY]);
+	$device = $database->escape_string($json[$REQUEST_DEVICE_KEY]);
+	$display = $database->escape_string($json[$REQUEST_DISPLAY_KEY]);
+	$fingerPrint = $database->escape_string($json[$REQUEST_FINGER_PRINT_KEY]);
+	$hardwade = $database->escape_string($json[$REQUEST_HARDWARE_KEY]);
+	$host = $database->escape_string($json[$REQUEST_HOST_KEY]);
+	$deviceId = $database->escape_string($json[$REQUEST_DEVICE_ID_KEY]);
+	$manufacturer = $database->escape_string($json[$REQUEST_MANUFACTURER_KEY]);
+	$product = $database->escape_string($json[$REQUEST_PRODUCT_KEY]);
+	$radio = $database->escape_string($json[$REQUEST_RADIO_KEY]);
+	$tags = $database->escape_string($json[$REQUEST_TAGS_KEY]);
+	$time = $database->escape_string($json[$REQUEST_TIME_KEY]);
+	$type = $database->escape_string($json[$REQUEST_TYPE_KEY]);
+	$unkown = $database->escape_string($json[$REQUEST_UNKNOWN_KEY]);
+	$user = $database->escape_string($json[$REQUEST_USER_KEY]);
+	$densityName = $database->escape_string($json[$REQUEST_DENSITY_NAME_KEY]);
+	$screenSize = $database->escape_string($json[$REQUEST_SCREEN_SIZE_KEY]);
+	$density = $database->escape_string($json[$REQUEST_DENSITY_KEY]);
+	$xdpi = $database->escape_string($json[$REQUEST_XDPI_KEY]);
+	$ydpi = $database->escape_string($json[$REQUEST_YDPI_KEY]);
 
 	$insertDeviceInformation =  "INSERT INTO $TABLE_NAME_DEVICE_INFORMATION (" .
 	"$REQUEST_VERSION_CODE_NAME_KEY, " .
@@ -220,8 +175,8 @@ isScreenDetailsSet($_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY])
 	"$REQUEST_SCREEN_SIZE_KEY, " .
 	"$REQUEST_DENSITY_KEY, " .
 	"$REQUEST_XDPI_KEY, " .
-	"$REQUEST_YDPI_KEY" .
-	"$REQUEST_TAGS_KEY" .
+	"$REQUEST_YDPI_KEY, " .
+	"$TABLE_COLUMN_NAME_COUNT " .
 	") VALUES (".
 	"'$versionCodeName', ".
 	"'$versionIncremental', ".
@@ -251,25 +206,66 @@ isScreenDetailsSet($_POST[$REQUEST_LANDSCAPE_SCREEN_DETAILS_KEY])
 	"'$screenSize', ".
 	"$density, ".
 	"$xdpi, ".
-	"$ydpi".
+	"$ydpi, ".
+	"0 ".
 	");";
 
 
-	$database->query($insertDeviceInformation);
-	$deviceInformationId = $database->insert_id;
+	$queryDeviceInformation =  "SELECT $TABLE_COLUMN_NAME_COUNT, $TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID FROM $TABLE_NAME_DEVICE_INFORMATION WHERE " .
+	"$REQUEST_VERSION_CODE_NAME_KEY = '$versionCodeName' AND " .
+	"$REQUEST_VERSION_INCREMENTAL_KEY = '$versionIncremental' AND " .
+	"$REQUEST_VERSION_RELEASE_KEY = '$versionRelease' AND " .
+	"$REQUEST_VERSION_SDK_STRING_KEY = '$versionSdkString' AND " .
+	"$REQUEST_VERSION_SDK_INTEGER_KEY= '$versionSdkInteger' AND " .
+	"$REQUEST_BOARD_KEY = '$board' AND " .
+	"$REQUEST_BOOT_LOADER_KEY = '$bootLoader' AND " .
+	"$REQUEST_BRAND_KEY = '$brand' AND " .
+	"$REQUEST_CPU_ABI_KEY = '$cpuAbi' AND " .
+	"$REQUEST_CPU_ABI_2_KEY = '$cpuAbi2' AND " .
+	"$REQUEST_DEVICE_KEY = '$device' AND " .
+	"$REQUEST_DISPLAY_KEY = '$display' AND " .
+	"$REQUEST_FINGER_PRINT_KEY = '$fingerPrint' AND " .
+	"$REQUEST_HARDWARE_KEY = '$hardwade' AND " .
+	"$REQUEST_HOST_KEY = '$host' AND " .
+	"$REQUEST_DEVICE_ID_KEY = '$deviceId' AND " .
+	"$REQUEST_MANUFACTURER_KEY = '$manufacturer' AND " .
+	"$REQUEST_PRODUCT_KEY = '$product' AND " .
+	"$REQUEST_RADIO_KEY = '$radio' AND " .
+	"$REQUEST_TAGS_KEY = '$tags' AND " .
+	"$REQUEST_TYPE_KEY = '$type' AND " .
+	"$REQUEST_UNKNOWN_KEY= '$unkown' AND " .
+	"$REQUEST_USER_KEY = '$user' AND " .
+	"$REQUEST_DENSITY_NAME_KEY = '$densityName' AND " .
+	"$REQUEST_SCREEN_SIZE_KEY = '$screenSize' AND " .
+//	"$REQUEST_TIME_KEY = '$time' AND " .
+	"$REQUEST_DENSITY_KEY = $density ".
+	
+	";";
 
-	$insertScreenSize = getScreenDetailsInsertQuery($database, $deviceInformationId, 1, $portraitScreenDetails);
-	$database->query($insertScreenSize);
+	$queryResults = $database->query($queryDeviceInformation);
+	if ($row = $queryResults->fetch_assoc()){
+		$count = $row[$TABLE_COLUMN_NAME_COUNT]+1;
+		$deviceInofmationId = $row[$TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID];
+		$insertCountIncrement = "UPDATE $TABLE_NAME_DEVICE_INFORMATION SET $TABLE_COLUMN_NAME_COUNT=$count WHERE $TABLE_COLUMN_NAME_DEVICE_INFORMATION_ID = $deviceInofmationId;";
+		$database->query($insertCountIncrement);
+	}else {
+		$database->query($insertDeviceInformation);
+		$deviceInformationId = $database->insert_id;
 
-	$insertScreenSize = getScreenDetailsInsertQuery($database, $deviceInformationId, 0, $landscapeScreenDetails);
-	$database->query($insertScreenSize);
+		$insertScreenSize = getScreenDetailsInsertQuery($database, $deviceInformationId, 1, $portraitScreenDetails);
+		$database->query($insertScreenSize);
+
+		$insertScreenSize = getScreenDetailsInsertQuery($database, $deviceInformationId, 0, $landscapeScreenDetails);
+		$database->query($insertScreenSize);
+	}
 
 	$database->close();
+
 
 	http_response_code(200);
 } else {
 	http_response_code(412);
-	die(Error::getJsonError(0));
+	die("Unable to process");
 }
 
 
