7e006c4dc08b818b34d948c253c968bbee1830b2
Shanjeef/JBYung <shanjeef.satchi@xtremelabs.com>
2013-06-12 20:11:07 -0400

    EmirH: fixed small bugs and formatted layout slightly

--- ScreenDeviceStatistics.php
+++ ScreenDeviceStatistics.php
@@ -1,4 +1,5 @@
 <?php
+$TITLE = "Android Device Screen Sizes";
 
 include 'Constants.php';
 
@@ -37,20 +38,17 @@ $query = "SELECT DISTINCT ".
 $database = new mysqli($URL, $USER_NAME, $PASSWORD, $DATABASE);
 $results = $database->query($query);
 
-?>
-<html>
-<body>
-
-<?php
-
 function calculateDp($pixel,$density){
 	return $dp = (int)($pixel / $density);
 }
 
-function getMeasurements ($yPixel, $xPixel, $density){
+function getMeasurements ($yPixel, $xPixel, $density, $newline = false){
 	$yDp = calculateDp($yPixel, $density);
 	$xDp = calculateDp($xPixel, $density);
-	return "$xPixel x $yPixel px ($xDp x $yDp dp)";
+	$newlineHtml = " ";
+	if ($newline)
+	$newlineHtml = "<br>";
+	return "$xPixel x $yPixel px$newlineHtml($xDp x $yDp dp)";
 }
 
 function getDpMeasurement ($pixel, $density){
@@ -74,15 +72,19 @@ function getScreenDetails($navBarHeight, $navBarWidth, $titleBarHeight, $statusB
 	// Navigation bar
 	$html .="<tr>";
 	$html .="<td>";
-	$html .="Navigation bar width";
-	$html .="</td>";
-	$html .="<td>";
+
 	if ($navBarHeight != 0){
+		$html .="Navigation bar height: ";
+		$html .="</td>";
+		$html .="<td>";
 		$navBarMeasurement = getMeasurement($navBarHeight, $density);
 		$html .=$navBarMeasurement;
-	}else if ($portraitNavBarWidth != 0){
+	}else if ($navBarWidth != 0){
+		$html .="Navigation bar width: ";
+		$html .="</td>";
+		$html .="<td>";
 		$navBarMeasurement = getMeasurement($navBarWidth, $density);
-		$html .=$navBarWidth;
+		$html .=$navBarMeasurement;
 	}else{
 		$html .="No navigation bar found";
 	}
@@ -94,7 +96,7 @@ function getScreenDetails($navBarHeight, $navBarWidth, $titleBarHeight, $statusB
 	// Title bar
 	$html .="<tr>";
 	$html .="<td>";
-	$html .="Title bar width";
+	$html .="Title bar width: ";
 	$html .="</td>";
 	$html .="<td>";
 	if ($titleBarHeight != 0){
@@ -111,7 +113,7 @@ function getScreenDetails($navBarHeight, $navBarWidth, $titleBarHeight, $statusB
 	// Status bar
 	$html .="<tr>";
 	$html .="<td>";
-	$html .="Status bar width";
+	$html .="Status bar width: ";
 	$html .="</td>";
 	$html .="<td>";
 	if ($statusBarHeight != 0){
@@ -129,10 +131,10 @@ function getScreenDetails($navBarHeight, $navBarWidth, $titleBarHeight, $statusB
 	// ContentView
 	$html .="<tr>";
 	$html .="<td>";
-	$html .="Content View";
+	$html .="Content View: ";
 	$html .="</td>";
 	$html .="<td>";
-	$statusBarMeasurement = getMeasurements($contentViewPixelWidth,$contentViewPixelHeight, $density);
+	$statusBarMeasurement = getMeasurements($contentViewPixelWidth,$contentViewPixelHeight, $density, true);
 	$html .=$statusBarMeasurement;
 
 	$html .="</td>";
@@ -144,13 +146,17 @@ function getScreenDetails($navBarHeight, $navBarWidth, $titleBarHeight, $statusB
 	return $html;
 }
 
+$menu = "";
+
+$content = "";
+$id = 0;
 while ($row = $results->fetch_assoc()){
 
 	$deviceName = $row[$REQUEST_DEVICE_NAME_KEY];
 	$density = $row[$REQUEST_DENSITY_KEY];
 	$densityName = $row[$REQUEST_DENSITY_NAME_KEY];
 	$screenSize = $row[$REQUEST_SCREEN_SIZE_KEY];
-	
+
 
 	$portraitDevicePixelHeight = $row[$TABLE_COLUMN_PORTRAIT_DEVICE_PIXEL_HEIGHT_KEY];
 	$portraitDevicePixelWidth = $row[$TABLE_COLUMN_PORTRAIT_DEVICE_PIXEL_WIDTH_KEY];
@@ -171,21 +177,43 @@ while ($row = $results->fetch_assoc()){
 	$landscapeTitleBarHeight = $row[$TABLE_COLUMN_LANDSCAPE_TITLE_BAR_HEIGHT_KEY];
 
 
-	$html = "<h1>$deviceName</h1>";
+	$menu .= "<li><a href='#Header$id'>$deviceName</a></li>";
+
+	$html = "<h2 id='Header$id'>$deviceName</h2>";
 	$deviceMeasurements = getMeasurements($portraitDevicePixelWidth, $portraitDevicePixelHeight, $density);
-	$html .= "<p>$deviceMeasurements</p>";
-	$html .= "<p>$densityName</p>";
-	$html .= "<p>$screenSize</p>";
-	
+	$html .= "<p class='Center'>$deviceMeasurements / ";
+	$html .= "$densityName / ";
+	$html .= "$screenSize</p>";
 
-	$html .= "<h2>Portrait Details<h2>";
+	$html .="<div class='ScreenDetails'>";
+	$html .="<div class='Table'>";
+	$html .= "<h2>Portrait Details</h2>";
 	$html .= getScreenDetails($portraitNavBarHeight, $portraitNavBarWidth, $portraitTitleBarHeight, $portraitStatusBarHeight, $portraitContentViewPixelWidth, $portraitContentViewPixelHeight, $density);
-	$html .= "<h2>Landscape Details<h2>";
+	$html .="</div>";
+	$html .="<div class='Table'>";
+	$html .= "<h2>Landscape Details</h2>";
 	$html .= getScreenDetails($landscapeNavBarHeight, $landscapeNavBarWidth, $landscapeTitleBarHeight, $landscapeStatusBarHeight, $landscapeContentViewPixelWidth, $landscapeContentViewPixelHeight, $density);
-
-	echo $html;
+	$html .="</div>";
+	$html .="<div class='Clear'></div>";
+	$html .="</div>";
+	$content .=$html;
+	$id ++;
 }
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<body>
+<div>
+<div>
+
+<h2><?php echo ($TITLE);?></h2>
+<ul>
+<?php echo($menu);?>
+</ul>
+</div>
+<div><?php echo($content); ?></div>
+</div>
+
 </body>
 </html>
 
